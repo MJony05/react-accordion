4bc8a063975c88e7324e79215f66cbd04045f3de
Jonibek <munirovjonibek@gmail.com>
2022-04-28 12:44:33 +0500

    react-accordion

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { index: "" };
+    this.data = [
+      { savol: "Html zormi", javob: "ha" },
+      { savol: "Html zormi", javob: "ha" },
+      { savol: "Html zormi", javob: "ha" },
+    ];
+  }
+  getIndex = (key) => {
+    this.setState({ index: key });
+  };
+  renderData = () => {
+    return this.data.map((val, key) => {
+      let active = this.state.index === key ? " active" : "";
+      return (
+        <div>
+          <div
+            className="ui styled accordion"
+            onClick={() => {
+              this.getIndex(key);
+            }}
+          >
+            <div className={"title" + active}>
+              <i className="dropdown icon"></i>
+              {val.savol}
+            </div>
+            <div className={"content" + active}>
+              <p
+                className="transition visible"
+                style={{ display: "block !important" }}
+              >
+                {val.javob}
+              </p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+  render() {
+    return this.renderData();
+  }
+}
+
+export default App;
